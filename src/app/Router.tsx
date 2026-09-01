6a73b64c85f89a7686f311e0a0c2b1c0d282138d
Data247Network <data247network@gmail.com>
2026-09-01 10:04:59 +0100

    Make Architecture v2 child home the default child dashboard

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -7,7 +7,8 @@ import { ActivityTrail } from '../screens/activity'
 import { FamilyHub } from '../screens/hub'
 import { EmergencyContacts } from '../screens/contacts'
 import { Filtering } from '../screens/filtering'
-import { ChildHome, ChildLock, ChildNotice } from '../screens/child'
+import { ChildLock, ChildNotice } from '../screens/child'
+import { ChildV2Home } from '../screens/ChildV2Home'
 import { ChildRoutinesV2, ChildRequestsV2, ChildRewardsV2 } from '../screens/ChildV2'
 import { SchoolModeV2 } from '../screens/SchoolModeV2'
 import { ChildSetup, RoleGate } from '../screens/setup'
@@ -18,6 +19,6 @@ import { Paywall, WebOverview, WebSplit } from '../screens/web'
 import { CloudFirstHome } from '../screens/CloudFirstHome'
 import { CloudFirstDevices } from '../screens/CloudFirstDevices'
 import { ParentV2 } from '../screens/ParentV2'
-const SCREENS: Record<ScreenId, () => JSX.Element> = { onboard1:()=> <Onboarding index={0}/>, onboard2:()=> <Onboarding index={1}/>, onboard3:()=> <Onboarding index={2}/>, login:LoginScreen, addChild:AddChild, enrollDevice:EnrollDevice, home:CloudFirstHome, v2control:ParentV2, schoolModeV2:SchoolModeV2, map:MapZones, geofence:NewGeofence, screentime:ScreenTime, scenario:ScenarioEditor, activity:Filtering, trail:ActivityTrail, contacts:EmergencyContacts, alerts:Alerts, acoustic:AcousticAlert, hub:FamilyHub, tips:SafetyTips, roleGate:RoleGate, pair:CloudFirstDevices, childSetup:ChildSetup, household:Household, plans:Plans, report:ActivityReport, childHome:ChildHome, childRoutines:ChildRoutinesV2, childRequests:ChildRequestsV2, childRewards:ChildRewardsV2, childLock:ChildLock, childNotice:ChildNotice, webOverview:WebOverview, webSplit:WebSplit, paywall:Paywall }
+const SCREENS: Record<ScreenId, () => JSX.Element> = { onboard1:()=> <Onboarding index={0}/>, onboard2:()=> <Onboarding index={1}/>, onboard3:()=> <Onboarding index={2}/>, login:LoginScreen, addChild:AddChild, enrollDevice:EnrollDevice, home:CloudFirstHome, v2control:ParentV2, schoolModeV2:SchoolModeV2, map:MapZones, geofence:NewGeofence, screentime:ScreenTime, scenario:ScenarioEditor, activity:Filtering, trail:ActivityTrail, contacts:EmergencyContacts, alerts:Alerts, acoustic:AcousticAlert, hub:FamilyHub, tips:SafetyTips, roleGate:RoleGate, pair:CloudFirstDevices, childSetup:ChildSetup, household:Household, plans:Plans, report:ActivityReport, childHome:ChildV2Home, childRoutines:ChildRoutinesV2, childRequests:ChildRequestsV2, childRewards:ChildRewardsV2, childLock:ChildLock, childNotice:ChildNotice, webOverview:WebOverview, webSplit:WebSplit, paywall:Paywall }
 function LoginScreen(){const {signIn}=useDevice();return <Login onSignedIn={signIn}/>}
 export function Screen({id}:{id:ScreenId}){const C=SCREENS[id];return <C/>}
